438ed8d50010bbe1d8a595dfe05f7776971aab32
TealGlow <alyssacomstock95@gmail.com>
2022-02-19 22:18:43 -0800

    created conv_endian func, started testing with positive decimal values

--- task.py
+++ task.py
@@ -16,4 +16,45 @@ def my_datetime(num_sec):
 
 
 def conv_endian(num, endian='big'):
-    pass
+    """
+    Function that takes a decimal value and which endian direction it is and
+    returns the hex value in whatever endian type it is.
+    :param num: decimal value
+    :param endian: what direction the hex value goes
+    :return: hex value result or None if the endian value is not valid
+    """
+
+    if endian != 'big' and endian != "little":
+        # check to see if the endian value is valid
+        # before anything is calculated
+        return None
+
+    temp = num
+    hex_nums = ''
+    result = []
+    hex_dict = {
+        0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'
+    }
+    
+    while temp != 0:
+        # Calculate the value first, and the direction will be
+        # reversed based on what endian direction it is
+        hex_nums += str(hex_dict[temp % 16])
+        temp = temp//16
+    for i in reversed(range(0, len(hex_nums), 2)):
+        # get each byte segment which is 2 digit values in the string
+        if len(hex_nums[i:i + 2]) < 2:
+            # if it is a single digit then a 0 is added
+            result.append("0" + hex_nums[i:i + 2])
+        else:
+            result.append(hex_nums[i:i + 2][::-1])
+
+    if endian == "little":
+        # if its little endian the values need to be reversed
+        result = result[::-1]
+    return " ".join(result)
+
+
+if __name__ == '__main__':
+    print(conv_endian(123))
+    print(conv_endian(954786))
--- tests.py
+++ tests.py
@@ -1,10 +1,55 @@
+"""
+Names:
+Date:
+Class: CS362 - Software Engineering II
+Assignment:
+Description:
+"""
+
 import unittest
+from task import *
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
+
+    def test_big_endian_posi_1(self):
+        """
+        Function that tests returning the big endian hex value of
+        a positive decimal number 954786.
+        Expected Hex value should be a string of '0E 91 A2'
+        """
+
+        self.assertEqual(conv_endian(954786), '0E 91 A2')
+
+    def test_big_endian_posi_2(self):
+        """
+        Function that tests returning the big endian hex value of
+        a positive decimal number 123.
+        Expected Hex value should be a string of '7B'
+        """
+
+        self.assertEqual(conv_endian(123), '7B')
+
+    def test_big_endian_posi_3(self):
+        """
+        Function that tests returning the big endian hex value of
+        a positive decimal number 1.
+        Expected Hex value should be a string of '01'
+        """
+
+        self.assertEqual(conv_endian(1), '01')
+
+    def test_little_endian_posi_1(self):
+        """
+        Function that tests returning the little endian hex value of
+        a positive decimal number 954786.
+        Expected Hex value should be a string of 'A2 91 0E'
+        """
+
+        self.assertEqual(conv_endian(954786, 'little'), 'A2 91 0E')
 
 
 if __name__ == '__main__':
